7bf44b9533ce0835ab4b71e321220e13e0783530
Muhammad umer <umershaikh217@gmail.com>
2023-01-24 17:26:30 +0500

    merged reference contracts

---
+++ hardhat/contracts/Reference contracts/protocol/libraries/logic/ReserveLogic.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.10;
+
+import {Errors} from '../helpers/Errors.sol';
+import {DataTypes} from '../types/DataTypes.sol';
+
+/**
+ * @title ReserveLogic library
+ * @notice Implements the logic to update the reserves state
+ */
+library ReserveLogic {
+  using ReserveLogic for DataTypes.ReserveData;
+
+  /**
+   * @notice Updates the liquidity cumulative index and the variable borrow index.
+   * @param reserve The reserve object
+   **/
+  function updateState(
+    DataTypes.ReserveData storage reserve
+  ) internal {
+    _updateIndexes(reserve);
+  }
+
+
+  /**
+   * @notice Updates the reserve indexes and the timestamp of the update.
+   * @param reserve The reserve reserve to be updated
+   **/
+  function _updateIndexes(
+    DataTypes.ReserveData storage reserve
+  ) internal {
+    reserve.lastUpdateTimestamp = uint40(block.timestamp);
+
+  }
+
+}
