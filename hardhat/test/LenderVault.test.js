2865775a1a74b8ba87bb9b8ab0344c9a97f7ce9d
Sing <16409791+singyiu@users.noreply.github.com>
2023-01-26 17:19:16 -0800

    redeem

--- hardhat/test/LenderVault.test.js
+++ hardhat/test/LenderVault.test.js
@@ -2,8 +2,8 @@ const { ethers } = require("hardhat");
 const { use, expect } = require("chai");
 
 let accounts;
-let lender0;
-let amount0;
+let lender0, lender1;
+let amount0, amount1;
 
 let mockERC20;
 let lenderVault;
@@ -12,7 +12,9 @@ before(async function () {
     // get accounts from hardhat
     accounts = await ethers.getSigners();
     lender0 = accounts[0];
+    lender1 = accounts[1];
     amount0 = ethers.utils.parseEther('100');
+    amount1 = ethers.utils.parseEther('100');
 });
   
 describe("LenderVault", function () {
@@ -31,12 +33,30 @@ describe("LenderVault", function () {
     });
 
     describe("deposit", function () {
-        it("Should be able to deposit into vault", async function () {
+        it("Lenders should be able to deposit into vault", async function () {
             await mockERC20.faucet(lender0.address, amount0);
-            await mockERC20.approve(lenderVault.address, amount0);
-            const tx = await lenderVault.deposit(amount0, lender0.address);
-            expect(tx).to.emit(lenderVault, 'Deposit').withArgs(lender0.address, lender0.address, amount0);
+            await mockERC20.connect(lender0).approve(lenderVault.address, amount0);
+            expect(await lenderVault.totalAssets()).to.equal(0);
+            expect(await lenderVault.balanceOf(lender0.address)).to.equal(0);
+            const tx0 = await lenderVault.connect(lender0).deposit(amount0, lender0.address);
+            expect(tx0).to.emit(lenderVault, 'Deposit').withArgs(lender0.address, lender0.address, amount0);
             expect(await lenderVault.balanceOf(lender0.address)).to.equal(amount0);
-        }) 
+            expect(await lenderVault.totalAssets()).to.equal(amount0);
+
+            await mockERC20.faucet(lender1.address, amount1);
+            await mockERC20.connect(lender1).approve(lenderVault.address, amount1);
+            expect(await lenderVault.balanceOf(lender1.address)).to.equal(0);
+            const tx1 = await lenderVault.connect(lender1).deposit(amount1, lender1.address);
+            expect(tx1).to.emit(lenderVault, 'Deposit').withArgs(lender1.address, lender1.address, amount1);
+            expect(await lenderVault.balanceOf(lender1.address)).to.equal(amount1);
+            expect(await lenderVault.totalAssets()).to.equal(amount0.add(amount1));
+        });
+    });
+
+    describe("redeem", function () {
+        it("Lenders should be able to redeem from vault", async function () {
+            const tx0 = await lenderVault.connect(lender0).redeem(amount0, lender0.address, lender0.address);
+            expect(tx0).to.emit(lenderVault, 'Withdraw').withArgs(lender0.address, lender0.address, lender0.address, amount0, amount0);            
+        });
     });
 });
