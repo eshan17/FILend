3e2bbb6070c938ffcbedc7c2a52ce134144b9cfe
fahimahmedx <68152521+fahimahmedx@users.noreply.github.com>
2023-01-28 02:11:04 -0500

    initial navbar

---
+++ front-end/components/Button.js
@@ -0,0 +1,7 @@
+export const Button=({text})=>{
+    return(
+        <div className="w-32 bg-blue-500 text-center px-2 py-2 text-white font-bold cursor-pointer rounded-xl">
+            {text}
+        </div>
+    )
+}
--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -1,7 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
   theme: {
+    colors: {
+      'fil-blue': '#0190ff',
+    },
     extend: {},
   },
   plugins: [],
