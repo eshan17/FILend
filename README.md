ef10cffc1d8ec153b4e6bedd11f8f52d7190bcf6
Muhammad umer <umershaikh217@gmail.com>
2023-01-29 00:25:12 +0500

    lending page

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
+<!-- @format -->
+
 # FILend
+
 Decentralized lending protocol for FVM Space Warp Hackathon 2023
+
+## Figma Design
+
+(Link)[https://www.figma.com/file/51fVYxckwjrOmvQkbX00RW/FVM-Hackathon?node-id=0%3A1&t=W9UlXJ2jswZMs1uk-1]
--- front-end/pages/_app.js
+++ front-end/pages/_app.js
@@ -1,5 +1,56 @@
-import '@/styles/globals.css'
+/** @format */
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import "../styles/globals.css";
+import "@rainbow-me/rainbowkit/styles.css";
+
+import {
+  RainbowKitProvider,
+  getDefaultWallets,
+  darkTheme,
+} from "@rainbow-me/rainbowkit";
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import { mainnet, polygon, optimism, arbitrum, goerli } from "wagmi/chains";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+
+const { chains, provider, webSocketProvider } = configureChains(
+  [
+    mainnet,
+    polygon,
+    optimism,
+    arbitrum,
+    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === "true" ? [goerli] : []),
+  ],
+  [
+    alchemyProvider({
+      // This is Alchemy's default API key.
+      // You can get your own at https://dashboard.alchemyapi.io
+      apiKey: "Ol2-sdEOu9L-vkvwuM9ZcTVZnDWxBqOT",
+    }),
+    publicProvider(),
+  ]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "RainbowKit App",
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+  webSocketProvider,
+});
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
 }
+
+export default MyApp;
