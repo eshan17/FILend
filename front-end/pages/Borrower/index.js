369a7df164e7669c47dc0dd755d8b9f8335f703b
Muhammad umer <umershaikh217@gmail.com>
2023-01-30 02:21:58 +0500

    Lending and Borrower Page (Disconnected)

--- front-end/pages/Borrower/index.js
+++ front-end/pages/Borrower/index.js
@@ -1,14 +1,49 @@
 /** @format */
+
 import React from "react";
 import Navbar from "@/components/Navbar";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
 import Fade from "react-reveal/Fade";
+import Image from "next/image";
+import img from "@/public/Images/img7.svg";
+import waves from "@/public/Images/waves.svg";
+import { useAccount } from "wagmi";
+import { useRouter } from "next/router";
 
 const index = () => {
+  const router = useRouter();
+  const { address, isConnecting, isDisconnected, isConnected } = useAccount();
+  useEffect(() => {
+    if (isConnected) {
+      router.push(`/Borrower/${address}`);
+    }
+  }, [isConnecting, isConnected]);
+
   return (
     <>
       <Fade top>
-        <Navbar button={false} />
+        <Navbar button={true} />
       </Fade>
+
+      <div className="container flex flex-col justify-center items-center  ">
+        <Fade bottom>
+          <div>
+            <Image src={img} alt="computer" width={400} height={400} />
+          </div>
+          <div className=" -mt-6"></div>
+
+          <div className="text-sm font-light leading-9 mt-6 text-gray-400  ">
+            Verify that you are a storage provider on the Filecoin Network
+          </div>
+
+          <div className="mt-6">
+            <button className="text-md font-semibold  bg-black px-6 py-2 text-white rounded-lg transition ease-in duration-150 text-sm hover:scale-110">
+              Verify
+            </button>
+          </div>
+        </Fade>
+        <Image src={waves} />
+      </div>
     </>
   );
 };
--- front-end/pages/Lender/index.js
+++ front-end/pages/Lender/index.js
@@ -2,13 +2,46 @@
 
 import React from "react";
 import Navbar from "@/components/Navbar";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
 import Fade from "react-reveal/Fade";
+import Image from "next/image";
+import img from "@/public/Images/img4.svg";
+import waves from "@/public/Images/waves.svg";
+import { useAccount } from "wagmi";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+// width={200}
 const index = () => {
+  const router = useRouter();
+  const { address, isConnecting, isDisconnected, isConnected } = useAccount();
+
+  useEffect(() => {
+    if (isConnected) {
+      router.push(`/Lender/${address}`);
+    }
+  }, [isConnecting, isConnected]);
+
   return (
     <>
       <Fade top>
         <Navbar button={false} />
       </Fade>
+
+      <div className="container flex flex-col justify-center items-center  ">
+        <Fade bottom>
+          <div>
+            <Image src={img} alt="computer" width={600} height={600} />
+          </div>
+          <div className=" -mt-6">
+            <ConnectButton />
+          </div>
+
+          <div className="text-sm font-light leading-9 mt-6 text-gray-400  ">
+            Connect your wallet to get started
+          </div>
+        </Fade>
+        <Image src={waves} />
+      </div>
     </>
   );
 };
