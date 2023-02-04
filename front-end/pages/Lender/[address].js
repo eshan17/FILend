45e12ca4422ee39aa526959760793cae9bbad5f8
Muhammad umer <umershaikh217@gmail.com>
2023-02-04 20:36:37 +0500

    lender-page changes

--- front-end/pages/Lender/[address].js
+++ front-end/pages/Lender/[address].js
@@ -12,7 +12,9 @@ import svg from "@/public/Images/loan.svg";
 import svg2 from "@/public/Images/vault.svg";
 import Image from "next/image";
 import LenderLoan from "@/components/LenderLoan";
-import { format, addMonths, parseJSDate } from "date-fns";
+import { format, addMonths } from "date-fns";
+// import axios from "axios";
+import axios from "axios";
 
 const address = () => {
   const router = useRouter();
@@ -37,27 +39,27 @@ const address = () => {
 
   const [months, setMonths] = useState(12);
   const [date, setDate] = useState("--/--/----");
-  const [duration, setDuration] = useState("12 months");
+  const [duration, setDuration] = useState("18 months");
   let opacity = 100;
 
   const currentdate = new Date().toLocaleDateString();
 
   const dateChange = (event) => {
+    event.preventDefault();
+
     if ("12 Months" === event.target.value) {
-      setMonths(18);
-      setDuration("18 months");
+      setMonths(12);
+      setDuration("12 months");
     }
 
     if ("18 Months" === event.target.value) {
-      setMonths(12);
-      setDuration("12 months");
+      setMonths(18);
+      setDuration("18 months");
     }
 
     const currentDate = new Date();
     const futureDate = addMonths(currentDate, months);
     const formattedDate = format(futureDate, "dd/MM/yyyy");
-    console.log("date " + formattedDate);
-
     setDate(formattedDate);
   };
 
@@ -71,10 +73,110 @@ const address = () => {
     setSeniorTotalReturn(event2.target.value * 1.5);
   };
 
+  const handleSubmit1 = (event) => {
+    event.preventDefault();
+    setShowModal(true);
+  };
+
+  const sendData = async (address, lender) => {
+    try {
+      const response = await axios.post("/api/lenders-data", {
+        address,
+        lender,
+      });
+
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  async function getData() {
+    try {
+      const response = await axios.get("/api/lenders-data");
+
+      console.log(response.data);
+      return response.data;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async function handleSubmit2(event) {
+    console.log("confirm button clicked");
+    event.preventDefault();
+    const body = {
+      Amount: j_amount,
+      LoanDuration: duration,
+      TotalReturn: j_totalReturn,
+      InterestRate: j_interestRate,
+      RepaymentDate: date,
+      LendingDate: currentdate,
+    };
+
+    
+    // sendData(address, body);
+
+    // try {
+    //   const sendresponse = await axios.post("/api/data", {
+    //     j_amount,
+    //   });
+
+    //   console.log("send" + sendresponse.data);
+    // } catch (error) {
+    //   console.error(error);
+    // }
+
+    // const data = getData();
+    // console.log("data = " + data);
+
+    // try {
+    //   const response = await fetch("/api/store-event-data", {
+    //     method: "POST",
+    //     headers: { "Content-Type": "application/json" },
+    //     body: JSON.stringify(body),
+    //   });
+    //   if (response.status !== 200) {
+    //     alert("Oops! Something went wrong . Please refresh and try again.");
+    //   } else {
+    //     console.log("Form successfully submitted!");
+    //     let responseJSON = await response.json();
+    //     console.log(responseJSON.cid);
+    //     console.log("Responce ", responseJSON);
+    //     axios
+    //       .get(`https://web3.storage/ipfs/${cid}`)
+    //       .then((response) => {
+    //         const data = response.data;
+    //         // use the data as needed
+    //       })
+    //       .catch((error) => {
+    //         console.error(error);
+    //       });
+
+    //     // console.log("Responce body ", responseJSON.body);
+    //     // console.log("Responce LoanDuration ", responseJSON.LoanDuration);
+    //     // console.log("Responce TotalReturn", responseJSON.TotalReturn);
+    //     // console.log("Responce InterestRate ", responseJSON.InterestRate);
+    //     // console.log("Responce RepaymentDate ", responseJSON.RepaymentDate);
+    //     // console.log("Responce LendingDat ", responseJSON.LendingDate);
+
+    //     // await createEvent(responseJSON.cid);
+    //   }
+    //   // check response, if success is false, dont take them to success page
+    // } catch (error) {
+    //   alert(
+    //     ` catch Oops! Something went wrong. Please refresh and try again. Error ${error}`
+    //   );
+    // }
+  }
+
   useEffect(() => {
     setTimeout(() => {
       setData({ message: "Data loaded" });
-
+      const currentDate = new Date();
+      const futureDate = addMonths(currentDate, 18);
+      const formattedDate = format(futureDate, "dd/MM/yyyy");
+      setDate(formattedDate);
       setLoading(false);
     }, 100);
 
@@ -82,12 +184,6 @@ const address = () => {
       router.push(`/Lender`);
     }
   }, [isConnecting, isConnected]);
-
-  if (showModal == true) {
-    opacity = 10;
-  } else {
-    opacity = 100;
-  }
 
   return (
     <>
@@ -167,65 +263,73 @@ const address = () => {
                     <>
                       {showModal && (
                         <>
-                          <div
-                            className={`bg-white py-5 blur-none -mt-8 h-96 z-50 md:filter-none 
+                          <Fade>
+                            <div
+                              className={`bg-white py-5 blur-none -mt-8 h-96 z-50 md:filter-none  
                               px-16 relative flex flex-col   w-modal space-y-2  border-2 border-black  shadow-black rounded-xl `}
-                          >
-                            {/* <h1 className="text-3xl justify-center items-center text-fil-primary font-bold">
+                            >
+                              {/* <h1 className="text-3xl justify-center items-center text-fil-primary font-bold">
                             Lend Loan
                           </h1> */}
 
-                            {/* Left side */}
-                            <div classname="  flex  border-black">
-                              <div className="text-black text-xl    ">
-                                <Image src={svg} width={30} height={30} />
-                              </div>
-
-                              <div className="text-black text-xl  font-semibold ">
-                                Loan Receipt
-                              </div>
-
-                              <div className="flex flex-row mt-4   space-x-6">
-                                <div className="flex flex-col   text-sm space-y-3  text-black font-medium">
-                                  <div>Amount</div>
-                                  <div>Loan Duration</div>
-                                  <div>Total Return</div>
-                                  <div>Interest Rate</div>
-                                  <div>Repayment Date</div>
-                                  <div>Lending Date</div>
+                              {/* Left side */}
+                              <div classname="  flex  border-black">
+                                <div className="text-black text-xl    ">
+                                  <Image src={svg} width={30} height={30} />
                                 </div>
 
-                                <div className="flex flex-col   text-sm space-y-3  text-black font-medium">
-                                  <div>{j_amount}</div>
-                                  <div>{duration}</div>
-                                  <div>{j_totalReturn}</div>
-                                  <div>{j_interestRate}</div>
-                                  <div>{date}</div>
-                                  <div>{currentdate}</div>
+                                <div className="text-black text-xl  font-semibold ">
+                                  Loan Receipt
                                 </div>
 
-                                <div className="  absolute top-24 right-4">
-                                  <Image src={svg2} width={220} height={300} />
+                                <div className="flex flex-row mt-4   space-x-6">
+                                  <div className="flex flex-col   text-sm space-y-3  text-black font-medium">
+                                    <div>Amount</div>
+                                    <div>Loan Duration</div>
+                                    <div>Total Return</div>
+                                    <div>Interest Rate</div>
+                                    <div>Repayment Date</div>
+                                    <div>Lending Date</div>
+                                  </div>
+
+                                  <div className="flex flex-col   text-sm space-y-3  text-black font-medium">
+                                    <div>{j_amount}</div>
+                                    <div>{duration}</div>
+                                    <div>{j_totalReturn}</div>
+                                    <div>{j_interestRate}</div>
+                                    <div>{date}</div>
+                                    <div>{currentdate}</div>
+                                  </div>
+
+                                  <div className="  absolute top-24 right-4">
+                                    <Image
+                                      src={svg2}
+                                      width={220}
+                                      height={300}
+                                    />
+                                  </div>
                                 </div>
                               </div>
+
+                              <form onSubmit={handleSubmit2}>
+                                <div className="flex flex-row w-full   justify-center pb-8 space-x-12 items-center transition ease-in duration-150 ">
+                                  <button
+                                    onClick={() => setShowModal(false)}
+                                    className="bg-black px-8 py-1 mt-10 text-white rounded-md"
+                                  >
+                                    Close
+                                  </button>
+
+                                  <button
+                                    type="submit"
+                                    className="bg-fil-primary px-8 py-1 mt-10 text-white rounded-md"
+                                  >
+                                    confirm
+                                  </button>
+                                </div>
+                              </form>
                             </div>
-
-                            <div className="flex flex-row w-full   justify-center pb-8 space-x-12 items-center ">
-                              <button
-                                onClick={() => setShowModal(false)}
-                                className="bg-black px-8 py-1 mt-10 text-white rounded-md"
-                              >
-                                Close
-                              </button>
-
-                              <button
-                                onClick={() => setShowModal(false)}
-                                className="bg-fil-primary px-8 py-1 mt-10 text-white rounded-md"
-                              >
-                                confirm
-                              </button>
-                            </div>
-                          </div>
+                          </Fade>
                         </>
                       )}
                     </>
@@ -235,10 +339,13 @@ const address = () => {
                   <div className={` flex  justify-center items-center z-10`}>
                     <div
                       className={`flex flex-col  ${
-                        showModal ? "blur" : ""
+                        showModal ? "blur mt-12" : ""
                       }  py-16 w-1/4 space-y-6 justify-center items-center  shadow-black rounded-xl -mt-24 bg-white shadow-md  `}
                     >
-                      <div className="space-y-6   justify-center items-center">
+                      <form
+                        className="space-y-6   justify-center items-center"
+                        onSubmit={handleSubmit1}
+                      >
                         <div className="flex  text-sm justify-between items-center space-x-10 text-black rounded-sm  bg-white     ">
                           <div className=" font-light  w-16"> Amount</div>
 
@@ -257,8 +364,7 @@ const address = () => {
                           <div className="font-light w-16"> Loan Duration</div>
 
                           <select
-                            id="duration"
-                            name="duration"
+                            required
                             className="border-2  border-black focus:border-fil-primary rounded-md px-4 py-1  font-medium"
                             onChange={dateChange}
                           >
@@ -269,13 +375,14 @@ const address = () => {
 
                         <div className="justify-center items-center flex ">
                           <button
-                            onClick={() => setShowModal(true)}
+                            // onClick={() => setShowModal(true)}
+                            type="submit"
                             className="text-md font-semibold  mt-8 bg-fil-secondary px-16 py-2 text-white rounded-lg transition ease-in duration-150 text-sm hover:scale-110"
                           >
                             Lend
                           </button>
                         </div>
-                      </div>
+                      </form>
                     </div>
                   </div>
                 </Fade>
