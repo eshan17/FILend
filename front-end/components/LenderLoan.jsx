271114e8b9d8f0aac38bd48b8d347417c387c43a
Muhammad umer <umershaikh217@gmail.com>
2023-02-06 00:46:47 +0500

    Lender-Flow-changes

--- front-end/components/LenderLoan.jsx
+++ front-end/components/LenderLoan.jsx
@@ -1,6 +1,7 @@
 /** @format */
 
 import React from "react";
+import Link from "next/link";
 
 const LenderLoan = ({
   SerialNo,
@@ -14,6 +15,7 @@ const LenderLoan = ({
   active,
   Tx,
 }) => {
+  console.log("Transaction Hash " + Tx);
   return (
     <>
       <div className="grid-cols-9 mt-8   px-4 grid text-black font-medium text-lg ">
@@ -26,23 +28,26 @@ const LenderLoan = ({
         <div>{EndingDate}</div>
         <div>
           {active ? (
-            <button className="text-md font-semibold  bg-puprle px-8 py-2 text-white rounded-lg transition ease-in duration-150 text-sm hover:scale-110">
+            <button className="text-md font-semibold  bg-puprle px-8 py-2 text-white rounded-lg transition ease-in duration-150 text-sm  ">
               {Status}
             </button>
           ) : (
-            <button className="text-md font-semibold  bg-fil-secondary px-6 py-2 text-white rounded-lg transition ease-in duration-150 text-sm hover:scale-110">
+            <button className="text-md font-semibold  bg-fil-secondary px-6 py-2 text-white rounded-lg transition ease-in duration-150 text-sm ">
               {Status}
             </button>
           )}
         </div>
         <div>
-          {" "}
-          <button
-            onClick={Tx}
-            className="text-md font-semibold  bg-fil-brown px-8 py-2 text-white rounded-lg transition ease-in duration-150 text-sm hover:scale-110"
-          >
-            Details
-          </button>
+          <div className="transition ease-in duration-150  hover:scale-110">
+            <Link
+              className="text-md font-semibold  bg-fil-brown px-8 py-3 text-white rounded-lg transition ease-in duration-150 text-sm hover:scale-110"
+              // href={`https://hyperspace.filfox.info/en/tx/${Tx}`}
+              href={`https://hyperspace.filfox.info/en/tx/${Tx}`}
+              target="_blank"
+            >
+              Details
+            </Link>
+          </div>
         </div>
       </div>
     </>
--- front-end/utils/connectContract.js
+++ front-end/utils/connectContract.js
@@ -1,19 +1,23 @@
 /** @format */
 
 import LoanManagerAbiJSON from "./loanManager.json";
+import juniorVaultAbiJSON from "./juniorVault.json";
+import seniorVaulAbiJSON from "./seniorVault.json";
+import mockErcAbiJSON from "./mockErc.json";
+
 import { ethers } from "ethers";
 
-async function connectContract() {
-  //Note: Your contractAddress will start with 0x, delete everything between the quotes and paste your contract address.
-  const lenderVaultAddress = "0xF57727359b8947eD163253D9c7721582a6614E82";
-  const mockErcAddress = "0xDDb6fCBd3e8922756a6Bb740775d12e46c217ea2";
-  const lenderVaultJunior = "0xa2DE18B14eEF924A6E85B3afc28944103264a377";
-  const lenderVaultSenior = "0xbaBC3e94092dCf1D52bc6dA709dc1674DaE71384";
-  const LoanManager = "0xc9045434354f2B82bcBC58880536F4F5E8508521";
+//Note: Your contractAddress will start with 0x, delete everything between the quotes and paste your contract address.
+const lenderVaultAddress = "0xF57727359b8947eD163253D9c7721582a6614E82";
+const mockErcAddress = "0x922BA67974f1e66F79Df0E8Ca12Da3FCAf336B06";
+const lenderVaultJunior = "0x26d98E799CCE56cb39FF8E02C66521437De23487";
+const lenderVaultSenior = "0xe45DcE80E9f6e7FD3d607a6817EbFaFCC2D21f9A";
+const LoanManager = "0x86d563B776Bb148D56b244eF21762957cc5b5A79";
 
-  const contractABI = LoanManagerAbiJSON.abi;
+export async function connectJuniorVault() {
+  const juniorVaultAbi = juniorVaultAbiJSON.abi;
 
-  let Contract;
+  let juniorVaultContract;
   try {
     const { ethereum } = window;
 
@@ -21,14 +25,137 @@ async function connectContract() {
       //checking for eth object in the window
       const provider = new ethers.providers.Web3Provider(ethereum);
       const signer = provider.getSigner();
-      Contract = new ethers.Contract(LoanManager, contractABI, signer); // instantiating new connection to the contract
+      juniorVaultContract = new ethers.Contract(
+        lenderVaultJunior,
+        juniorVaultAbi,
+        signer
+      ); // instantiating new connection to the contract
     } else {
       console.log("Ethereum object doesn't exist!");
     }
   } catch (error) {
     console.log("ERROR:", error);
   }
-  return Contract;
+  return juniorVaultContract;
 }
 
-export default connectContract;
+export async function connectlenderVault() {
+  const lenderVaultAbi = juniorVaultAbiJSON.abi;
+
+  let lenderVaultContract;
+  try {
+    const { ethereum } = window;
+
+    if (ethereum) {
+      //checking for eth object in the window
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
+      lenderVaultContract = new ethers.Contract(
+        lenderVaultAddress,
+        lenderVaultAbi,
+        signer
+      ); // instantiating new connection to the contract
+    } else {
+      console.log("Ethereum object doesn't exist!");
+    }
+  } catch (error) {
+    console.log("ERROR:", error);
+  }
+  return lenderVaultContract;
+}
+
+export async function connectSeniorVault() {
+  const SeniorVaultAbiJSON = seniorVaulAbiJSON.abi;
+
+  let SeniorVaultContract;
+  try {
+    const { ethereum } = window;
+
+    if (ethereum) {
+      //checking for eth object in the window
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
+      SeniorVaultContract = new ethers.Contract(
+        lenderVaultSenior,
+        SeniorVaultAbiJSON,
+        signer
+      ); // instantiating new connection to the contract
+    } else {
+      console.log("Ethereum object doesn't exist!");
+    }
+  } catch (error) {
+    console.log("ERROR:", error);
+  }
+  return SeniorVaultContract;
+}
+
+export async function connectmockErc() {
+  const mockErcAbi = mockErcAbiJSON.abi;
+
+  let mockErcContract;
+  try {
+    const { ethereum } = window;
+
+    if (ethereum) {
+      //checking for eth object in the window
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
+      mockErcContract = new ethers.Contract(mockErcAddress, mockErcAbi, signer); // instantiating new connection to the contract
+    } else {
+      console.log("Ethereum object doesn't exist!");
+    }
+  } catch (error) {
+    console.log("ERROR:", error);
+  }
+  return mockErcContract;
+}
+
+export async function connectLoanManager() {
+  const loancontractABI = LoanManagerAbiJSON.abi;
+
+  let LenderContract;
+  try {
+    const { ethereum } = window;
+
+    if (ethereum) {
+      //checking for eth object in the window
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
+      LenderContract = new ethers.Contract(
+        LoanManager,
+        loancontractABI,
+        signer
+      ); // instantiating new connection to the contract
+    } else {
+      console.log("Ethereum object doesn't exist!");
+    }
+  } catch (error) {
+    console.log("ERROR:", error);
+  }
+  return LenderContract;
+}
+
+// async function connectContract() {
+//   const loancontractABI = LoanManagerAbiJSON.abi;
+
+//   let LenderContract;
+//   try {
+//     const { ethereum } = window;
+
+//     if (ethereum) {
+//       //checking for eth object in the window
+//       const provider = new ethers.providers.Web3Provider(ethereum);
+//       const signer = provider.getSigner();
+//       LenderContract = new ethers.Contract(
+//         LoanManager,
+//         loancontractABI,
+//         signer
+//       ); // instantiating new connection to the contract
+//     } else {
+//       console.log("Ethereum object doesn't exist!");
+//     }
+//   } catch (error) {
+//     console.log("ERROR:", error);
+//   }
+//   return LenderContract;
+// }
